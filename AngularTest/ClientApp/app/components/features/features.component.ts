de47af8cb989b7bb359ebfb48427d141c2c66f90
Alexis Cedeño <alacedro@hotmail.com>
2018-03-09 11:56:29 -0600

    Nav repair

--- AngularTest/ClientApp/app/components/features/features.component.ts
+++ AngularTest/ClientApp/app/components/features/features.component.ts
@@ -1,5 +1,5 @@
 ﻿import { Component, Inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http'; 
+import { HttpClient } from '@angular/common/http';
 
 @Component({
     selector: 'features',
